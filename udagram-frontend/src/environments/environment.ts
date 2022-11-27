82c6b35a08f59a5fd5b2a69dc28f97fb0e42995f
nguyễn hoàng minh quân <quanhk511@gmail.com>
2022-11-27 22:54:39 +0700

    update FE

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab155c754874c4187bdf2a604bc404ff-314825875.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
